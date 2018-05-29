59a67ccf7b1f8d74aeb045d4615adfdef4693860
Hossam shaheen <hossameldinshaheen@gmail.com>
2018-05-30 00:52:16 +0200

    adding 2 numbers

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var a = 'Hello World!';
 
-function b() {
+function adding(){
+    
+    // Num Variable 
+    let a = 2,
+        b = 3;
+    
+    // returning a plus b
+    return a + b;
     
 }
+
+document.querySelector("h1").innerHTML = adding();
